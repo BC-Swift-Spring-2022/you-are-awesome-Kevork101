6edff278b6e1733179f31b8236c2eae913ca9e69
Kevork Atinizian <katinizian@gmail.com>
2022-02-07 02:55:15 -0500

    Sounds

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,7 @@ class ViewController: UIViewController {
     var imageNumber = 0
     var messageNumber = 0
     let totalNumberOfImages = 2
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,17 +35,55 @@ class ViewController: UIViewController {
                         "Is Borat awesome?...NOT",
                         "I hate building apps?...NOT",
                         "This my seester"]
+        var newMessage = messages[Int.random(in: 0...messages.count-1)]
+        while messageLabel.text == newMessage {
+            newMessage = messages[Int.random(in: 0...messages.count-1)]
+        }
+        messageLabel.text = newMessage
+        //messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
         
+        var newImage = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+        while imageView.image == newImage {
+            newImage = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+        }
+        imageView.image = newImage
         
-        messageLabel.text = messages[Int.random(in:0...messages.count-1)]
-        //imageView.image = UIImage(named:"image\(Int.random(in: 0...totalNumberOfImages))")
-        //var number = int.random(in:)
-        let result = Int.random(in:0...1)
-        if result == 1{
-                imageView.image = UIImage(named: "When-Borat-Says-My-Wife")
-        }else{
-            imageView.image = UIImage(named: "How-I-Actually-Look-Infront-Of-My-Crush")
+        if let sound = NSDataAsset(name: "sound0"){
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            }catch{
+                print("ERROR")
             }
+            }else {
+            print("ERROR")
+        }
+        
+       // imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+        
+//        messageLabel.text = messages[messageNumber]
+//        messageNumber+=1
+//        if messageNumber == messages.count{
+//            messageNumber = 0
+//        }
+//
+//        let imageName = "image" + String(imageNumber)
+//        imageView.image = UIImage(named: imageName)
+//        imageNumber = imageNumber + 1
+//        if imageNumber == totalNumberOfImages{
+//            imageNumber = 0
+//        }
+        //let imageName = "image\(imageNumber)"
+        
+//        messageLabel.text = messages[Int.random(in:0...messages.count-1)]
+//        //imageView.image = UIImage(named:"image\(Int.random(in: 0...totalNumberOfImages))")
+//        //var number = int.random(in:)
+//        let result = Int.random(in:0...1)
+//        if result == 1{
+//                imageView.image = UIImage(named: "When-Borat-Says-My-Wife")
+//        }else{
+//            imageView.image = UIImage(named: "How-I-Actually-Look-Infront-Of-My-Crush")
+//            }
     }
     
 }
